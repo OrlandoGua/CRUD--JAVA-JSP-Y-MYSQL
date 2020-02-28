be4703c6b1dab00ee34780a602142edd1da47530
OrlandoGua <56378541+OrlandoGua@users.noreply.github.com>
2020-02-28 12:58:44 -0600

    Add files via upload

---
+++ control_clientes.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-02-2020 a las 19:51:59
+-- Versión del servidor: 10.4.6-MariaDB
+-- Versión de PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `control_clientes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `saldo` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `nombre`, `apellido`, `email`, `telefono`, `saldo`) VALUES
+(2, 'Orlando', 'Guatemala', 'orlando_sistemas@hotmail.com', '2345623332', 550),
+(11, 'Tanya', 'Guatemala', 'mark34@hotmail.com', '294-124-4455', 100);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/main/java/web/ServletControlador.java
@@ -0,0 +1,171 @@
+package web;
+
+import datos.ClienteDaoJDBC;
+import dominio.Cliente;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/ServletControlador")
+public class ServletControlador extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String accion = request.getParameter("accion");
+
+        if (accion != null) {
+            switch (accion) {
+                case "editar":
+                    this.editarCliente(request, response);
+                    break;
+                case "eliminar":
+                    this.eliminarCliente(request, response);
+                    break;
+                
+                default:
+                    this.accionDefault(request, response);
+                    break;
+
+            }
+        } else {
+            this.accionDefault(request, response);
+        }
+    }
+
+    private void accionDefault(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Cliente> cliente = new ClienteDaoJDBC().listar();
+        System.out.println("clientes =" + cliente);
+        HttpSession sesion = request.getSession();
+        
+       
+        //compatimos en el el alcance de session ya que request al usar forward vuelve al cargar los mismo datos al volver ainsertar
+        sesion.setAttribute("clientes", cliente);
+        sesion.setAttribute("totalClientes", cliente.size());
+        sesion.setAttribute("saldoTotal", this.calcularSaldoTotal(cliente));
+        // request.getRequestDispatcher("clientes.jsp").forward(request, response);
+        response.sendRedirect("clientes.jsp");//este accion si notifica al navegador
+
+    }
+
+    private double calcularSaldoTotal(List<Cliente> clientes) {
+        double saldoTotal = 0;
+        for (Cliente cliente : clientes) {
+            saldoTotal += cliente.getSaldo();
+        }
+        return saldoTotal;
+    }
+
+    private void editarCliente(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+     //recuperar el idCliente
+     
+     int idCliente =Integer.parseInt(request.getParameter("idCliente"));
+     Cliente cliente = new ClienteDaoJDBC().encontrar(new Cliente(idCliente));
+     request.setAttribute("cliente", cliente);
+     String jspEditar="/WEB-INF/paginas/cliente/editarCliente.jsp";
+     request.getRequestDispatcher(jspEditar).forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String accion = request.getParameter("accion");
+
+        if (accion != null) {
+            switch (accion) {
+                case "insertar":
+                    this.insertarCliente(request, response);
+                    break;
+                case "modificar":
+                      this.modificarCliente(request,response);
+                      break;
+                default:
+                    this.accionDefault(request, response);
+                    break;
+
+            }
+        } else {
+            this.accionDefault(request, response);
+        }
+    }
+
+      private void modificarCliente(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        //recuperamos los valores del formulario agregar cliente
+        
+        int idCliente = Integer.parseInt(request.getParameter("idCliente"));
+        
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String email = request.getParameter("email");
+        String telefono = request.getParameter("telefono");
+        double saldo = 0;
+        String saldoString = request.getParameter("saldo");
+        if (saldoString != null && !"".equals(saldoString)) {
+            saldo = Double.parseDouble(saldoString);
+        }
+
+        //Creamos el objeto de cliente (modelo)
+        Cliente cliente = new Cliente(idCliente,nombre, apellido, email, telefono, saldo);
+
+        //Modificar en la base de datos
+        int registrosModificados = new ClienteDaoJDBC().actualizar(cliente);
+        System.out.println("registrosModificados = " + registrosModificados);
+
+        //rediregimos a la accion por deaul
+        this.accionDefault(request, response);
+
+    }
+      private void insertarCliente(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        //recuperamos los valores del formulario agregar cliente
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String email = request.getParameter("email");
+        String telefono = request.getParameter("telefono");
+        double saldo = 0;
+        String saldoString = request.getParameter("saldo");
+        if (saldoString != null && !"".equals(saldoString)) {
+            saldo = Double.parseDouble(saldoString);
+        }
+
+        //Creamos el objeto de cliente (modelo)
+        Cliente cliente = new Cliente(nombre, apellido, email, telefono, saldo);
+
+        //insertamos en la base de datos
+        int registrosModificados = new ClienteDaoJDBC().insertar(cliente);
+        System.out.println("registrosModificados = " + registrosModificados);
+
+        //rediregimos a la accion por deaul
+        this.accionDefault(request, response);
+
+    }
+
+private void eliminarCliente(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        //recuperamos los valores del formulario agregar cliente
+        
+        int idCliente = Integer.parseInt(request.getParameter("idCliente"));
+        
+        
+
+        //Creamos el objeto de cliente (modelo)
+        Cliente cliente = new Cliente(idCliente);
+
+        //Eliminamos en la base de datos
+        int registrosModificados = new ClienteDaoJDBC().eliminar(cliente);
+        System.out.println("registrosModificados = " + registrosModificados);
+
+        //rediregimos a la accion por deaul
+        this.accionDefault(request, response);
+
+    }
+}
